b82167a16743ee79f17c43009d27656e7d8cb345
Fahmid Sakib <fahmidsakib97@gmail.com>
2022-09-26 17:24:15 +0600

    Updated

--- dbConfig.js
+++ dbConfig.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.URL);
+const sequelize = new Sequelize(process.env.URL, {dialect: 'postgres'});
 
 module.exports = sequelize
